a16c809bbfb60984bf711523396c4be9e6b9148f
[yash kharte] <[kharteyash03@gmail.com]>
2024-03-30 22:05:52 +0530

    user actions done

--- src/templates/admin-pages/AddImages.js
+++ src/templates/admin-pages/AddImages.js
@@ -26,10 +26,46 @@ export default function AddImages() {
     images();
   },[])
 
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [localPath, setLocalPath] = useState('');
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+    setLocalPath(URL.createObjectURL(file));
+  };
+console.log("selectedFile",localPath)
+  const handleUpload = async () => {
+    try {
+      if (!selectedFile) {
+        throw new Error('No file selected');
+      }
+
+      const formData = new FormData();
+      formData.append('image', selectedFile);
+
+      const response = await fetch('http://${IP}:5000/api/admin/view/gallery/addImage', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to upload image');
+      }
+
+      const data = await response.json();
+      console.log('Upload successful:', data);
+      // Handle success, update UI, etc.
+    } catch (error) {
+      console.error('Error uploading image:', error.message);
+      // Handle error, display error message, etc.
+    }
+  };
 
   return (
     <div>
-        <input type="file" />
+        <input type="file" onChange={handleFileChange}/>
+        <input type="submit" onSubmit={handleUpload} />
         <div className="d-flex flex-wrap justify-content-center align-items-center">
         {allImages?.data?.map((value, index) => {
           return (
--- src/templates/admin-pages/AllUsers.js
+++ src/templates/admin-pages/AllUsers.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { IP } from "../constants";
 import WMTable from "../../ui-components/table";
-
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { IconButton } from "@mui/material";
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 export default function AllUsers() {
   //http://localhost:5000/api/admin/view/users
   const [allUserDetails, setAllUserDetails] = useState({});
@@ -28,34 +31,101 @@ export default function AllUsers() {
     details();
   }, []);
 
+  const handleDeleteDialog = async(event, data) => {
+    console.log(data?._id);
+    try {
+        const response = await fetch(`http://${IP}:5000/api/admin/view/users/${data?._id}/delete`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // body: JSON.stringify(details),
+          credentials: "include",
+        });
+  
+        const userDelete = await response.json();
+        console.log("user deleted", userDelete);
+        // if (!userDetails?.data?.refreshToken) {
+        //   navigate("/");
+        // }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+  };
+  const handleEditDialog = async(event, data) => {
+    console.log(data?._id);
+    try {
+        const response = await fetch(`http://${IP}:5000/api/admin/view/users/${data?._id}/makeadmin`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          // body: JSON.stringify(details),
+          credentials: "include",
+        });
+  
+        const userAdmin = await response.json();
+        console.log("User admin", userAdmin);
+        // if (!userDetails?.data?.refreshToken) {
+        //   navigate("/");
+        // }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+  };
+
   const columns = [
     {
-        accessorKey:"firstName",
-        header:"First Name"
+      accessorKey: "firstName",
+      header: "First Name",
     },
     {
-        accessorKey:"lastName",
-        header:"Last Name"
+      accessorKey: "lastName",
+      header: "Last Name",
     },
     {
-        accessorKey:"email",
-        header:"Email"
+      accessorKey: "email",
+      header: "Email",
     },
     {
-        accessorKey:"role",
-        header:"Role"
+      accessorKey: "role",
+      header: "Role",
     },
-  ]
+    {
+      header: "Actions",
+      accessorKey: "actions",
+      Cell: ({ row }) => (
+        <>
+          {row?.original?.role === "user" ? (
+            <>
+              <IconButton>
+                <DeleteIcon
+                  onClick={(e) => handleDeleteDialog(e, row?.original)}
+                />
+              </IconButton>
+              <IconButton>
+                <AddCircleOutlineIcon onClick={(e) => handleEditDialog(e, row?.original)} />
+              </IconButton>
+            </>
+          ) : (
+            <></>
+          )}
+        </>
+      ),
+    },
+  ];
 
   console.log("all Users", allUserDetails);
 
   return (
     <div>
-      <WMTable 
-        columns={columns}
-        data={allUserDetails}
-        tableTitle={"All Users"}
-      />
+      {allUserDetails?.data && (
+        <WMTable
+          columns={columns}
+          data={allUserDetails?.data}
+          tableTitle={"All Users"}
+        />
+      )}
     </div>
   );
 }
