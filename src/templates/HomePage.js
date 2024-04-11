61282439207f745a4aa3fb54e691e86e7c117a08
[yash kharte] <[kharteyash03@gmail.com]>
2024-04-11 16:31:42 +0530

    get all notifications

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "../templates/css/NavigationBar.css";
 import { IP } from "./constants";
-
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import {IconButton, Menu, MenuItem} from "@mui/material"
 export default function HomePage() {
 const [userDetails, setUserDetails] = useState({})
+const [allNotifications, setAllNotifications] = useState({})
+const [menuAnchorEl, setMenuAnchorEl] = useState(null);
+const [isMenuOpen, setIsMenuOpen] = useState(false);
   const details = async () => {
     try {
       const response = await fetch(`http://${IP}:5000/api/users/get-details`, {
@@ -21,13 +25,52 @@ const [userDetails, setUserDetails] = useState({})
       console.error("Error:", error);
     }
   };
+
+
+  const notifications = async () => {
+    try {
+      const response = await fetch(`http://${IP}:5000/api/users/view/notifications`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // body: JSON.stringify(details),
+        credentials: "include",
+      });
+
+      const data = await response.json();
+      setAllNotifications(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  const handleOpenNotifications = (event) => {
+    setMenuAnchorEl(event.currentTarget);
+    setIsMenuOpen(true);
+  };
+console.log("allNotifications",allNotifications)
   useEffect(()=>{
     details();
+    notifications();
   },[])
 
-  console.log("userDetails",userDetails)
   return (
-    <div>
+    <div style={{position:"relative"}}>
+      <IconButton style={{position:"absolute", top:"10px", right:"10px", fontSize:"60px"}} onClick={(e)=>handleOpenNotifications(e)}>
+      <NotificationsIcon style={{fontSize:"30px"}}/>
+      </IconButton>
+      <Menu
+        anchorEl={menuAnchorEl}
+        open={isMenuOpen}
+        onClose={() => setIsMenuOpen(false)}
+      >
+        {allNotifications?.data?.map((value, index) => {
+          return(
+            <MenuItem>{value?.message}</MenuItem>
+          )
+        })}
+      </Menu>
+      <p style={{position:"absolute", top:"10px", right:"14px"}}>{allNotifications?.data?.length}</p>
       <div className="">
         {/* <div style={{ width:"100%", backgroundColor:"aqua"}}>
           <div style={{backgroundColor:"red", width:"40%", float:"left"}}>
